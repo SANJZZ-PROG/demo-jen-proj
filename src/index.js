6ecdf7d5ce5ab080a6c79376fedcf0e5c5e0504e
SANJZZ-PROG <sanjukthav@gmail.com>
2026-06-17 13:38:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,4 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
